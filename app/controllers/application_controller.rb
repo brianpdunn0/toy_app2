43d855e88733d5be00b81e199394c8a351b4e6be
Brian Dunn <brian@danceoftehheart.com>
2017-06-08 09:42:40 +0000

    Various dummy edits...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "Getting cloooooosssahhh..."
+    render html: "Getting closahhh..."
   end
 end
