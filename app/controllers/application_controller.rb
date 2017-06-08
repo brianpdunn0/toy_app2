f8f2df9cdfdeb6406c658d47212cfe87ce515295
Brian Dunn <brian@danceoftehheart.com>
2017-06-08 09:46:43 +0000

    Make it sound American

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "Getting closahhh..."
+    render html: "Getting closer and closer..."
   end
 end
